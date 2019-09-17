237c4dc4d67a9eefe038ce1d24d03669313838a6
Lucio <luchopelle8@gmail.com>
2019-09-17 17:01:07 -0300

    asd

---
+++ GccApplication1/GccApplication1/Debug/fnqueue.d
@@ -0,0 +1,16 @@
+fnqueue.d fnqueue.o: .././fnqueue.cpp \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ C:\Users\Lucio\Documents\GitHub\Embebidos\GccApplication1\GccApplication1\Librerias/fnqueue.h \
+ C:\Users\Lucio\Documents\GitHub\Embebidos\GccApplication1\GccApplication1\Librerias/critical.h
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdbool.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+C:\Users\Lucio\Documents\GitHub\Embebidos\GccApplication1\GccApplication1\Librerias/fnqueue.h:
+
+C:\Users\Lucio\Documents\GitHub\Embebidos\GccApplication1\GccApplication1\Librerias/critical.h:
