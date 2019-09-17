650864ee3332f7d3d2105677259fd52f14a2951e
Lucio <luchopelle8@gmail.com>
2019-09-17 16:50:38 -0300

    first commit

---
+++ GccApplication1/GccApplication1/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.cpp
+ *
+ * Created: 17-Sep-19 4:50:20 PM
+ * Author : Lucio
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
