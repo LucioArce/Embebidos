b4b66c57f20bf2d2e89c15a249e7b35beadb0e34
Lucio <luchopelle8@gmail.com>
2019-09-21 14:46:54 -0300

    comit "serio"

---
+++ GccApplication1/GccApplication1/Librerias/driverADC.h
@@ -0,0 +1,9 @@
+struct adc_cfg
+{
+	uint8_t canal;
+	void (callback*)(int);
+	uint8_t valor;
+	uint8_t idle;
+	
+};
+int adc_init(adc_cfg *cfg);
---
+++ GccApplication1/GccApplication1/sensor.cpp
@@ -0,0 +1,6 @@
+/*
+ * sensor.cpp
+ *
+ * Created: 20-Sep-19 5:41:39 PM
+ *  Author: Lucio
+ */ 
